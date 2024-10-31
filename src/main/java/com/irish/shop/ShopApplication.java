5e45ed217ddeffc146f4b19932a95bd5d48d25ef
Ihor Kruchynskyi <irishdash@Ihors-MacBook-Air.local>
2024-10-31 09:54:15 -0400

    initial

---
+++ src/main/java/com/irish/shop/ShopApplication.java
@@ -0,0 +1,36 @@
+package com.irish.shop;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+		System.out.println("Shop Application started!");
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+}
+
+
